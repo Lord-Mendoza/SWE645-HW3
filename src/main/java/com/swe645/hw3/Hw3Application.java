7be34873783bf421304319c6573fe6237fd7be67
Lord Mendoza <lord.d.mendoza@gmail.com>
2024-07-16 21:19:41 -0400

    Pushing missing servlet initializer

--- src/main/java/com/swe645/hw3/Hw3Application.java
+++ src/main/java/com/swe645/hw3/Hw3Application.java
@@ -2,19 +2,18 @@
 Lord Mendoza
 SWE 645
 
-This class is the main entrypoint
+This class is the main entrypoint of springboot.
  */
 
 package com.swe645.hw3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Hw3Application {
-
+public class Hw3Application extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(Hw3Application.class, args);
 	}
-
 }
